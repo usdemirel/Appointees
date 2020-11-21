04eb1b7f263932e26bb3f72711c95fe1be857baa
usdemirel <usaserkan@gmail.com>
2020-11-21 13:24:43 -0600

    //abc

--- src/main/java/com/notsecure/Appointees/entity/Client.java
+++ src/main/java/com/notsecure/Appointees/entity/Client.java
@@ -1,9 +1,16 @@
 package com.notsecure.Appointees.entity;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
 public class Client {
  @Id
  private Long id;
@@ -11,47 +18,8 @@ public class Client {
  private String email;
  private String phone;
  private String address;
+ //abc
 
-public Client() {
-}
-
-public Long getId() {
- return id;
-}
-
-public void setId(Long id) {
- this.id = id;
-}
-
-public String getName() {
- return name;
-}
-
-public void setName(String name) {
- this.name = name;
-}
-
-public String getEmail() {
- return email;
-}
-
-public void setEmail(String email) {
- this.email = email;
-}
-
-public String getPhone() {
- return phone;
-}
-
-public void setPhone(String phone) {
- this.phone = phone;
-}
-
-public String getAddress() {
- return address;
-}
-
-public void setAddress(String address) {
- this.address = address;
-}
+ 
+ 
 }
